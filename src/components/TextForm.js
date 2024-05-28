886493d2be61a029f45958b698a4f581f385907a
TakshaSharma <taksha.sharma08@gmail.com>
2024-05-28 12:41:54 +0530

    react-app

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,16 +22,17 @@ export default function TextForm(props) {
     // setText("new text");
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', 
+        color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
         </div>
         <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
         <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
         <button className='btn btn-primary mx-2' onClick={handleClearText}>Clear Text</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>Your text summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
